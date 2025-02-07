82aa5a85c249e39ecc88063496a848ab962c6070
Donlistar.eth <dev_sebasfc@outlook.es>
2025-02-07 17:06:45 -0500

    Añadien archivo JS funcional con close and open of nav

---
+++ main.js
@@ -0,0 +1,8 @@
+function closetMenuMobile(){
+    let boton = document.getElementById("nav_pc");
+    boton.style.display = "none";
+}
+function openMenuMobile(){
+    let boton = document.getElementById("nav_pc");
+    boton.style.display = "flex";
+}
