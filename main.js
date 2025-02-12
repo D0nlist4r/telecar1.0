e2ed7567e02d2d5de92f2196d35a3c665cad61bd
Donlistar.eth <dev_sebasfc@outlook.es>
2025-02-12 17:24:43 -0500

    creado footer, añadiendo slider and add more this for stetyc

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+//funcion para darle movimiento al slider 
+let index = 0;
+        function moveSlider() {
+            index = (index + 1) % 3;
+            document.getElementById('slider').style.transform = `translateX(-${index * 100}%)`;
+        }
+        setInterval(moveSlider, 3000);
+// Funciones para interactuar con el menu Mobile
 function closetMenuMobile(){
     let boton = document.getElementById("nav_pc");
     boton.style.display = "none";
