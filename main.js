18a560adf33a607adbdca58c9586de9d1208a946
Donlistar.eth <dev_sebasfc@outlook.es>
2025-02-26 19:33:21 -0500

    cambios interface

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 
 //funcion para darle movimiento al slider 
-let index = 0;
+    let index = 0;
         function moveSlider() {
-            index = (index + 1) % 3;
+
+            index = (index + 1) % 2;
             document.getElementById('slider').style.transform = `translateX(-${index * 100}%)`;
         }
-        setInterval(moveSlider, 3000);
+        setInterval(moveSlider, 5000);
+
+        let indexo = 0;
+
+        function moveSlidernav() {
+
+            indexo = (indexo + 1) % 2;
+            document.getElementById('sliders').style.transform = `translateX(-${indexo * 100}%)`;
+        }
+        setInterval(moveSlidernav, 5000);
+
 // Funciones para interactuar con el menu Mobile
 function closetMenuMobile(){
     let boton = document.getElementById("nav_pc");
